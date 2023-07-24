e063d7ddf1e7e7974e4e668d2bf40e48eeab9aa4
Koushik Mondal <darkdeavil004@gmail.com>
2023-07-24 10:29:11 +0530

    Bottom tab navigation style done

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,15 +3,30 @@ import HomeScreen from "./screens/HomeScreen";
 import { Entypo } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import ProfileScreen from "./screens/ProfileScreen";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
+
 function BottomTabs() {
   return (
-    <Tab.Navigator>
-   
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor:"rgba(0,0,0,0.9)",
+          position: "absolute",
+          bottom: 0,
+          left:0,
+          right:0,
+          shadowOpacity:4,
+          shadowRadius:4,
+          elevation:4,
+          shadowOffset: { width: 0, height: -4 },
+          borderTopWidth: 0,
+        },
+      }}
+    >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
@@ -21,9 +36,9 @@ function BottomTabs() {
           tabBarLabelStyle: { color: "white" },
           tabBarIcon: ({ focused }) =>
             focused ? (
-              <Entypo name="home" size={24} color="black" />
+              <Entypo name="home" size={24} color="white" />
             ) : (
-              <AntDesign name="home" size={24} color="black" />
+              <AntDesign name="home" size={24} color="white" />
             ),
         }}
       />
@@ -37,9 +52,9 @@ function BottomTabs() {
           tabBarLabelStyle: { color: "white" },
           tabBarIcon: ({ focused }) =>
             focused ? (
-                <Ionicons name="person" size={24} color="black" />
+              <Ionicons name="person" size={24} color="white" />
             ) : (
-                <Ionicons name="person-outline" size={24} color="black" />
+              <Ionicons name="person-outline" size={24} color="white" />
             ),
         }}
       />
@@ -47,16 +62,19 @@ function BottomTabs() {
   );
 }
 
-const Stack= createNativeStackNavigator();
-function Navigation(){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Main" component={BottomTabs} options={{headerShown:false}}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+const Stack = createNativeStackNavigator();
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-
-export default Navigation
+export default Navigation;
